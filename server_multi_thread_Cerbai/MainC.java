38a0bb2cf5197e91d3ea40de13c450375a5efbe6
SashaCerbai <75943464+SashaCerbai@users.noreply.github.com>
2021-10-20 11:22:04 +0200

    Delete server_multi_thread_Cerbai directory

--- server_multi_thread_Cerbai/MainC.java
+++
@@ -1,9 +0,0 @@
-package it.gabriele;
-
-public class MainC {
-    public static void main( String[] args ){
-        Multiclient cliente= new Multiclient();
-        cliente.connetti();
-        cliente.comunica();
-    }
-}
